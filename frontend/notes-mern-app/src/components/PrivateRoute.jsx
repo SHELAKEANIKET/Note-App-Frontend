833d2870cffe26f5a35f9e67999a8e3b417aeebb
Aniket Shelake <97660224+SHELAKEANIKET@users.noreply.github.com>
2024-05-19 11:45:24 +0530

    Update PrivateRoute.jsx

--- frontend/notes-mern-app/src/components/PrivateRoute.jsx
+++ frontend/notes-mern-app/src/components/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ element: Element, ...rest }) => {
   const  user  = localStorage.getItem("token");
-  console.log(user)
+  
   return user ? <Element {...rest} /> : <Navigate to="/login" />;
 };
 
